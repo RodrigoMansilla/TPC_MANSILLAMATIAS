a7213033b954948389863bb137aaa220a6232d24
RodrigoMansilla <rodri_gnr@hotmail.com.ar>
2019-07-16 17:23:25 -0300

    nuevo fondo y encabezado

---
+++ AceesoDatos/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace AceesoDatos
+{
+    public class AccesoDatosManager
+    {
+        public static string cadenaConexion = "data source= localhost\\SQLEXPRESS; initial catalog=MansillaRodrigo_DB; integrated security=sspi";
+        private SqlCommand comando;
+        private SqlConnection conexion;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+        public SqlCommand Comando
+        {
+            get { return comando; }
+        }
+
+        public AccesoDatosManager()
+        {
+            conexion = new SqlConnection(cadenaConexion);
+        }
+
+        public void setearConsulta(string consulta)
+        {
+            comando = new SqlCommand();
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        
+        public void setearSP(string sp)
+        {
+            comando = new SqlCommand();
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.CommandText = sp;
+        }
+
+        public void abrirConexion()
+        {
+            try
+            {
+                conexion.Open();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void cerrarConexion()
+        {
+            try
+            {
+                conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void ejecutarAccion()
+        {
+            try
+            {
+                comando.Connection = conexion;
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public int ejecutarAccionReturn()
+        {
+            try
+            {
+                comando.Connection = conexion;
+                return (int)comando.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void ejecutarConsulta()
+        {
+            try
+            {
+                comando.Connection = conexion;
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public abstract class ConnectionToSql
+        {
+            private readonly string connetion;
+            public ConnectionToSql()
+            {
+                connetion = "data source= localhost\\SQLEXPRESS; initial catalog=MansillaRodrigo_DB; integrated security=sspi"; 
+
+            }
+            protected SqlConnection GetConnection()
+            {
+                return new SqlConnection(connetion);
+
+            }
+        }
+
+        
+    }
+}
